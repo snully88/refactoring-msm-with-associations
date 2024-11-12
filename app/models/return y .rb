f0c5df07aa572b3e181ceb2530f26b082b90b609
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-11-12 23:38:55 +0000

    save?

--- app/models/return y .rb
+++ app/models/return y .rb
@@ -15,4 +15,15 @@
 class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
+
+  belongs_to(:zebra, class_name: "Director", foreign_key: "director_id")
+end
+
+def zebra
+x = self.director_id
+
+  matching_records = Director.where({ :id => x})
+  y = matching_records.first
+
+  return y #the row from the directors table
 end
